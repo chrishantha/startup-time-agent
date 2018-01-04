f543416d3c133b3cac3f2871d156f14431357511
M. Isuru Tharanga Chrishantha Perera <chrishantha@gmail.com>
2018-01-04 13:58:31 +0530

    Use Javassist

--- src/main/java/com/github/chrishantha/agent/java/startuptime/Agent.java
+++ src/main/java/com/github/chrishantha/agent/java/startuptime/Agent.java
@@ -1,33 +1,100 @@
 package com.github.chrishantha.agent.java.startuptime;
 
-import net.bytebuddy.agent.builder.AgentBuilder;
-import net.bytebuddy.implementation.MethodDelegation;
-import net.bytebuddy.matcher.ElementMatchers;
+import javassist.ClassPool;
+import javassist.CtClass;
+import javassist.CtMethod;
 
 import java.lang.instrument.Instrumentation;
-import java.lang.management.ManagementFactory;
-import java.net.SocketAddress;
+import java.util.concurrent.TimeUnit;
 
 public class Agent {
 
-    public static void premain(String arg, Instrumentation instrumentation) {
-        long mainStartTime = System.currentTimeMillis();
-        long vmStartTime = ManagementFactory.getRuntimeMXBean().getStartTime();
-        long mainUptime = ManagementFactory.getRuntimeMXBean().getUptime();
-        System.out.println(String.format("Agent - VM Start Time: %d", vmStartTime));
-        System.out.println(String.format("Agent - Main Start Time: %d", mainStartTime));
-        System.out.println(String.format("Agent - Main Uptime (ms): %d", mainUptime));
-        System.out.println(String.format("Agent - VM Startup Time (ms): %d", mainStartTime - vmStartTime));
-        new AgentBuilder.Default()
-//                .with(AgentBuilder.Listener.StreamWriting.toSystemOut())
-//                .type(ElementMatchers.nameEndsWith("Address"))
-//                .type(ElementMatchers.nameStartsWith("java.net"))
-                .type(ElementMatchers.named("io.netty.bootstrap.AbstractBootstrap"))
-                .transform((builder, typeDescription, classLoader, javaModule) ->
-                        builder.method(ElementMatchers.named("bind").and(ElementMatchers.takesArguments(SocketAddress.class)))
-//                        builder.method(ElementMatchers.any())
-                                .intercept(MethodDelegation.to(Interceptor.class) //.andThen(SuperMethodCall.INSTANCE)
-                                ))
-                .installOn(instrumentation);
+    private static final String NETTY_CLASS = "io/netty/bootstrap/AbstractBootstrap";
+
+    public static void premain(String agentArgs, Instrumentation inst) {
+        MeasurementSingleton.getInstance().premainEnter();
+//        long mainStartTime = System.currentTimeMillis();
+//        long vmStartTime = ManagementFactory.getRuntimeMXBean().getStartTime();
+//        long mainUptime = ManagementFactory.getRuntimeMXBean().getUptime();
+//        System.out.println(String.format("Agent - VM Start Time: %d", vmStartTime));
+//        System.out.println(String.format("Agent - Main Start Time: %d", mainStartTime));
+//        System.out.println(String.format("Agent - Main Uptime (ms): %d", mainUptime));
+//        System.out.println(String.format("Agent - VM Startup Time (ms): %d", mainStartTime - vmStartTime));
+//        new AgentBuilder.Default()
+////                .with(AgentBuilder.Listener.StreamWriting.toSystemOut())
+////                .type(ElementMatchers.nameEndsWith("Address"))
+////                .type(ElementMatchers.nameStartsWith("java.net"))
+//                .disableClassFormatChanges()
+////                .with(AgentBuilder.RedefinitionStrategy.RETRANSFORMATION)
+//                .type(ElementMatchers.named("io.netty.bootstrap.AbstractBootstrap"))
+////                .transform((builder, typeDescription, classLoader, javaModule) ->
+////                        builder.method(ElementMatchers.named("bind").and(ElementMatchers.takesArguments(SocketAddress.class)))
+////                                .intercept(MethodDelegation.to(Interceptor.class) //.andThen(SuperMethodCall.INSTANCE)
+////                                ))
+//
+//                .transform((builder, typeDescription, classLoader, javaModule) ->
+//                        builder.visit(Advice.to(TimeAdvice.class)
+//                                .on(ElementMatchers.named("bind").and(ElementMatchers.takesArguments(SocketAddress.class)))))
+//                .installOn(inst);
+
+
+//        TypePool typePool = TypePool.Default.ofClassPath();
+//
+//        new ByteBuddy().rebase(typePool.describe("io.netty.bootstrap.AbstractBootstrap").resolve(),
+//                ClassFileLocator.ForClassLoader.ofClassPath())
+//                .method(ElementMatchers.named("bind").and(ElementMatchers.takesArguments(SocketAddress.class)))
+//                .intercept(MethodDelegation.to(Interceptor.class)).make().load(ClassLoader.getSystemClassLoader());
+
+
+//        LongAdder totalTime = new LongAdder();
+
+        inst.addTransformer((classLoader, s, aClass, protectionDomain, bytes) -> {
+            long startTime = System.nanoTime();
+            try {
+                if (NETTY_CLASS.equals(s)) {
+                    long start = System.nanoTime();
+//                    // Javassist
+                    try {
+                        ClassPool cp = ClassPool.getDefault();
+                        CtClass cc = cp.get("io.netty.bootstrap.AbstractBootstrap");
+                        CtMethod m = cc.getDeclaredMethod("bind", new CtClass[]{cp.get("java.net.SocketAddress")});
+//                        CtMethod m = cc.getDeclaredMethod("bind");
+//                    m.addLocalVariable("elapsedTime", CtClass.longType);
+//                    m.insertBefore("elapsedTime = System.currentTimeMillis();");
+//                    m.insertAfter("{elapsedTime = System.currentTimeMillis() - elapsedTime;"
+//                            + "System.out.println(\"Method Executed in ms: \" + elapsedTime);}");
+
+//                        m.insertAfter("{ System.out.println(\"Server started. Current Uptime (ms): \" + " +
+//                                "java.lang.management.ManagementFactory.getRuntimeMXBean().getUptime());}");
+
+                        m.insertAfter("{com.github.chrishantha.agent.java.startuptime." +
+                                "MeasurementSingleton.getInstance().serverStarted();}");
+                        byte[] byteCode = cc.toBytecode();
+                        cc.detach();
+                        return byteCode;
+                    } catch (Exception ex) {
+                        ex.printStackTrace();
+                    } finally {
+                        MeasurementSingleton.getInstance()
+                                .setClassTransformationDuration(TimeUnit.NANOSECONDS
+                                        .toMillis(System.nanoTime() - start));
+//                        System.out.println(String.format("Agent - Transformation Time (ms): %d", TimeUnit.NANOSECONDS.toMillis(System.nanoTime() - start)));
+                    }
+                }
+                return null;
+            } finally {
+//                totalTime.add(System.nanoTime() - startTime);
+            }
+        });
+//
+//        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+//            long sum = totalTime.sum();
+//            System.out.println(String.format("Total Transformation Time: %d ms (%d ns)", TimeUnit.NANOSECONDS.toMillis(sum), sum));
+//        }));
+//
+//        System.out.println(String.format("Agent - Install Time (ms): %d", System.currentTimeMillis() - mainStartTime));
+//
+//        System.out.println(String.format("Agent - End Uptime (ms): %d", ManagementFactory.getRuntimeMXBean().getUptime()));
+        MeasurementSingleton.getInstance().premainExit();
     }
 }
---
+++ src/main/java/com/github/chrishantha/agent/java/startuptime/MeasurementSingleton.java
@@ -0,0 +1,109 @@
+package com.github.chrishantha.agent.java.startuptime;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.IOException;
+import java.lang.management.ManagementFactory;
+import java.lang.management.RuntimeMXBean;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.Arrays;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
+
+public final class MeasurementSingleton {
+
+    private static final MeasurementSingleton INSTANCE = new MeasurementSingleton();
+
+    private final RuntimeMXBean runtimeMXBean = ManagementFactory.getRuntimeMXBean();
+
+    private final long VM_START_TIME = runtimeMXBean.getStartTime();
+
+    private long agentStartNanoTime;
+
+    private long agentStartupTime;
+
+    private long agentEntryUptime;
+
+    private long agentDuration;
+
+    private long classTransformationDuration;
+
+    private long serverStartNanoTime;
+
+    private long serverStartupDuration;
+
+    private long serverStartupTime;
+
+    private long serverStartUptime;
+
+    private MeasurementSingleton() {
+        System.out.println(runtimeMXBean.getUptime());
+        System.out.println(System.currentTimeMillis() - VM_START_TIME);
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            Path p = Paths.get("log.csv");
+            File file = p.toFile();
+            if (!file.exists()) {
+                try (BufferedWriter writer = Files.newBufferedWriter(p)) {
+                    writer.write(getHeader());
+                    writer.newLine();
+                    writer.write(getValues());
+                    writer.newLine();
+                } catch (IOException e) {
+                    System.err.format("IOException: %s%n", e);
+                }
+            } else {
+                try (BufferedWriter writer = Files.newBufferedWriter(p,
+                        StandardOpenOption.APPEND)) {
+                    writer.write(getValues());
+                    writer.newLine();
+                } catch (IOException e) {
+                    System.err.format("IOException: %s%n", e);
+                }
+            }
+        }));
+    }
+
+    private String getHeader() {
+        return String.join(",", "Agent Startup Time (ms)", "Agent Entry Uptime (ms)",
+                "Agent Duration (ms)", "Class Transformation Duration (ms),Server Startup Time (ms),Server Start Uptime (ms),Server Startup Duration (ms)");
+    }
+
+    private String getValues() {
+        Long values[] = {agentStartupTime, agentEntryUptime, agentDuration, classTransformationDuration,
+                serverStartupTime, serverStartUptime, serverStartupDuration};
+        return Arrays.stream(values).map(i -> String.valueOf(i)).collect(Collectors.joining(","));
+    }
+
+    public static void main(String[] args) {
+        System.out.println(MeasurementSingleton.getInstance().getValues());
+    }
+
+    public static MeasurementSingleton getInstance() {
+        return INSTANCE;
+    }
+
+    public void premainEnter() {
+        agentStartNanoTime = System.nanoTime();
+        agentEntryUptime = runtimeMXBean.getUptime();
+        agentStartupTime = System.currentTimeMillis() - VM_START_TIME;
+    }
+
+    public void premainExit() {
+        agentDuration = TimeUnit.NANOSECONDS.toMillis(System.nanoTime() - agentStartNanoTime);
+        serverStartNanoTime = System.nanoTime();
+    }
+
+    public void serverStarted() {
+        serverStartupDuration = TimeUnit.NANOSECONDS.toMillis(System.nanoTime() - serverStartNanoTime)
+                - classTransformationDuration;
+        serverStartupTime = System.currentTimeMillis() - VM_START_TIME;
+        serverStartUptime = runtimeMXBean.getUptime();
+    }
+
+    public void setClassTransformationDuration(long classTransformationDuration) {
+        this.classTransformationDuration = classTransformationDuration;
+    }
+}
